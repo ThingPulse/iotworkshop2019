8bea9e28cd586afbd615f02e29dd4ec38e42f7b0
Daniel Eichhorn <dani.eichhorn@squix.ch>
2019-09-03 08:44:30 +0200

    Add logging and adjust configuration for debugging. Enable PSRAM.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  log_v("Hello world: %d", millis());
+  log_d("Free PSRAM: %d", ESP.getFreePsram());
+  log_i("Free heap: %d", ESP.getFreeHeap());
+  log_w("Total PSRAM: %d", ESP.getPsramSize());
+  log_e("Total heap: %d", ESP.getHeapSize());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }

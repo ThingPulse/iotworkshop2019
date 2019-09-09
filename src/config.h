22249c6c40080e7e6bd7ed9e3aac3d04241f69e2
Daniel Eichhorn <dani.eichhorn@squix.ch>
2019-09-09 08:13:37 +0200

    Cleanup

--- src/config.h
+++ src/config.h
@@ -1,3 +1,3 @@
-const char * ssid = "Gobblin";
-const char * wifipw = "luisa2012";
-const char * apikey = "o.eJZRlvei8Ck5BAyz7EhLjjMAzo186ZFi";
+const char * ssid = "";
+const char * wifipw = "";
+const char * apikey = "";
